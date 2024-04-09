4ee91649679b8ce65fa7776a579ea884be2478aa
Livia <livsantos.c@gmail.com>
2024-04-09 10:45:41 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"status": 200, "message": "API do Gustavo Da silva Castro Farias"})
+@app.route("/aleatorios")
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
+@app.route("/argumentos/<string:nome>")
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
+if __name__ == '__main__':
+    app.run(debug=True)
